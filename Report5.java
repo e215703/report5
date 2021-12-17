b788b5b9100b286819c9e83fdfe6e91306be3110
Taisei Maetake <e215703@ie.u-ryukyu.ac.jp>
2021-12-17 19:49:15 +0900

    step1 commit

---
+++ Report5.java
@@ -0,0 +1,11 @@
+public class Report5{
+    String str;
+    public Report5(String str){
+        this.str = str;
+    }
+
+    public static void main(String[] args) { 
+        String str = null;
+        System.out.println(str.length());
+    }
+}
