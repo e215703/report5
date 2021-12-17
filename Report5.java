716c32f4249e5bf01cb3ed3b19739bacf765c92f
Taisei Maetake <e215703@ie.u-ryukyu.ac.jp>
2021-12-17 20:13:23 +0900

    step3 commit

--- Report5.java
+++ Report5.java
@@ -1,12 +1,6 @@
 public class Report5{
     public static void main(String[] args) { 
-        try{
-            String str = null;
-            System.out.println(str.length());
-        } catch(NullPointerException e){
-            System.out.println("エラー発生:NullPointerException");
-            System.out.println(e.getMessage());
-        }
-        
+        String str = "百二十三";
+        System.out.println(Integer.parseInt(str));
     }
 }
