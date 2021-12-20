f147f82dc5206db1b1c352563828455296ab7556
Taisei Maetake <e215703@ie.u-ryukyu.ac.jp>
2021-12-20 16:57:24 +0900

    step3 modified

--- Report5.java
+++ Report5.java
@@ -1,6 +1,7 @@
 public class Report5{
     public static void main(String[] args) { 
         String str = "百二十三";
-        System.out.println(Integer.parseInt(str));
+        int value = Integer.parseInt(str);
+        System.out.println(value);
     }
 }
